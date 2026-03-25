1b29cb2a39c73b92da1b5216a373378242687a61
Pankaj-Patel-2911 <patelpanku098@gmail.com>
2026-03-25 18:06:25 +0530

    feature added in blog service getAll blogs

---
+++ services/blog/src/controllers/blog.ts
@@ -0,0 +1,44 @@
+import TryCatch from "../utils/TryCatch.js";
+import { sql } from "../utils/db.js";
+
+export const getAllBlogs = TryCatch(async (req, res) => {
+  const searchQuery = req.query.searchQuery as string | undefined;
+  const category = req.query.category as string | undefined;
+
+  let blogs;
+
+  if (searchQuery && category) {
+    blogs = await sql`
+      SELECT * FROM blogs 
+      WHERE 
+        (title ILIKE ${"%" + searchQuery + "%"} 
+        OR description ILIKE ${"%" + searchQuery + "%"}) 
+        AND category = ${category}
+      ORDER BY created_at DESC
+    `;
+  } else if (searchQuery) {
+    blogs = await sql`
+      SELECT * FROM blogs 
+      WHERE 
+        (title ILIKE ${"%" + searchQuery + "%"} 
+        OR description ILIKE ${"%" + searchQuery + "%"})
+      ORDER BY created_at DESC
+    `;
+  } else if (category) {
+    blogs = await sql`
+      SELECT * FROM blogs 
+      WHERE category = ${category}
+      ORDER BY created_at DESC
+    `;
+  } else {
+    blogs = await sql`
+      SELECT * FROM blogs 
+      ORDER BY created_at DESC
+    `;
+  }
+
+  res.json({
+    count: blogs.length,
+    blogs,
+  });
+});
